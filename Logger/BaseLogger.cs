73084e82af381fe78ee6dbf396db37244c01d7fa
Tom-Rohr <trohr92@live.com>
2023-01-20 17:53:21 -0800

    Added auto-property 'ClassName' to BaseLogger class. Created FileLogger class derived from BaseLogger class.

--- Logger/BaseLogger.cs
+++ Logger/BaseLogger.cs
@@ -2,6 +2,8 @@
 {
     public abstract class BaseLogger
     {
+        public string? ClassName { get; set; } //Auto-property
+
         public abstract void Log(LogLevel logLevel, string message);
     }
 }
