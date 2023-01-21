4f74ce3e0d6cb6dc6fc21c35dd3d1be87be4e708
Tom-Rohr <trohr92@live.com>
2023-01-20 16:25:42 -0800

    Created test file for testing for C# version 11.0 and testing if nullability is enabled

---
+++ Logger.Tests/ProgramPropertiesTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger.Tests
+{
+    internal class Class1
+    {
+    }
+}
