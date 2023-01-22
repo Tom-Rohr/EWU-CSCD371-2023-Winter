6793087328b50dbb0c37a540ceedfc1c45dfc9db
Tom-Rohr <trohr92@live.com>
2023-01-22 12:43:48 -0800

    Methods now throw NullArgumentExceptions if logger is null

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -1,24 +1,42 @@
-﻿namespace Logger
+﻿using System;
+
+namespace Logger
 {
     public static class BaseLoggerMixins //todo
     {
         public static void Error(this BaseLogger logger, string message, string[] args)
         {
+            if (logger is null)
+            {
+                throw new ArgumentNullException();
+            }
             string.Format(message, args);
             logger.Log(LogLevel.Error, message);
         }
         public static void Warning(this BaseLogger logger, string message, string[] args)
         {
+            if (logger is null)
+            {
+                throw new ArgumentNullException();
+            }
             string.Format(message, args);
             logger.Log(LogLevel.Warning, message);
         }
         public static void Information(this BaseLogger logger, string message, string[] args)
         {
+            if (logger is null)
+            {
+                throw new ArgumentNullException();
+            }
             string.Format(message, args);
             logger.Log(LogLevel.Information, message);
         }
         public static void Debug( this BaseLogger logger, string message, string[] args)
         {
+            if (logger is null)
+            {
+                throw new ArgumentNullException();
+            }
             string.Format(message, args);
             logger.Log(LogLevel.Debug, message);
         }
