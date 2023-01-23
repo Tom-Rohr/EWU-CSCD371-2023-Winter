165d3a610707e29b7c2be28e1d10539218a12199
Tom-Rohr <trohr92@live.com>
2023-01-23 09:00:59 -0800

    created unit tests for LogFactory class

--- Logger.Tests/LogFactoryTests.cs
+++ Logger.Tests/LogFactoryTests.cs
@@ -6,6 +6,44 @@ namespace Logger.Tests
     [TestClass]
     public class LogFactoryTests
     {
-        
+        [TestMethod]
+        public void LogFactory_ConfigureFileLogger_testLogFilePathValid()
+        {
+            string testPath = Path.Combine(Directory.GetCurrentDirectory(), "testLog.txt");
+
+            LogFactory factory = new LogFactory();
+            factory.ConfigureFileLogger(testPath);
+
+            Assert.AreEqual(true, Path.Exists(testPath));
+        }
+        [TestMethod]
+        public void LogFactory_ConfigureFileLogger_testLogFilePathInvalid()
+        {
+            string testPath = Path.Combine(Directory.GetCurrentDirectory(), "nonExistantFile.txt");
+
+            LogFactory factory = new LogFactory();
+            factory.ConfigureFileLogger(testPath);
+
+            Assert.AreEqual(false, Path.Exists(testPath));
+        }
+        [TestMethod]
+        public void LogFactory_CreateLogger_ReturnsLoggerIfConfigured()
+        {
+            string testPath = Path.Combine(Directory.GetCurrentDirectory(), "testLog.txt");
+
+            LogFactory factory = new LogFactory();
+            factory.ConfigureFileLogger(testPath);
+            BaseLogger logger = factory.CreateLogger("FileLogger");
+
+            Assert.IsNotNull(logger);
+            Assert.AreEqual(logger.ClassName, "FileLogger");
+        }
+        [TestMethod]
+        public void LogFactory_CreateLogger_ReturnsNullIfNotConfigured()
+        {
+            LogFactory factory = new LogFactory();
+
+            Assert.AreEqual(null, factory.CreateLogger("FileLogger"));
+        }
     }
 }
