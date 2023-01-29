7f917b3a277a9255a7f589d5c53e59f1d6684b91
Tom-Rohr <trohr92@live.com>
2023-01-29 12:46:41 -0800

    changed Main to call Geek-Jokes api fewer times

--- CanHazFunny/CanHazFunny/Program.cs
+++ CanHazFunny/CanHazFunny/Program.cs
@@ -8,11 +8,9 @@ class Program
     {
         //Feel free to use your own setup here - this is just provided as an example
         //new Jester(new SomeReallyCoolOutputClass(), new SomeJokeServiceClass()).TellJoke();
-        JokeService jokeService = new();
-        FunnyOut jokeWriter = new();
-        Jester joker = new(jokeService, jokeWriter);
-        
-        for(int i=0; i<10; i++)
+        Jester joker = new(new JokeService(), new FunnyOut());
+
+        for (int i=0; i<5; i++)
         {
             joker.TellJoke();
         }
