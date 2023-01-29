2807b9b4068ffda8eb8e8d2fb2ea07c96aabab48
Tom-Rohr <trohr92@live.com>
2023-01-28 20:39:41 -0800

    Added Zach's Jester unit tests. Refactored with TestInitialize method

--- CanHazFunny/CanHazFunny.Tests/JesterTests.cs
+++ CanHazFunny/CanHazFunny.Tests/JesterTests.cs
@@ -1,5 +1,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System;
+using System.Diagnostics.Metrics;
+
 namespace CanHazFunny.Tests;
 
 [TestClass]
@@ -7,25 +10,26 @@ public class JesterTests
 {
     private string testString = "";
     private Jester testJester = new();
-    [ClassInitialize]
-    public void TestSetup(TestContext testcontext)
+    private JokeService jokeService = new();
+    private FunnyOut jokeWriter = new();
+
+    [TestInitialize]
+    public void TestSetup()
     {
         TestCleanup();
-
-        var mockJokeService = new Mock<JokeService>();
-        FunnyOut jokeWriter = new(); 
-
-        mockJokeService.Setup(x => x.GetJoke()).Returns(testString);
-        testJester = new(mockJokeService.Object, jokeWriter);
+        testJester = new(jokeService, jokeWriter);
     }
+
     [TestCleanup]
-    public void TestCleanup() 
+    public void TestCleanup()
     {
         testString = "";
         testJester = new();
+        jokeService = new();
+        jokeWriter = new();
     }
 
-    /*[TestMethod]
+    [TestMethod]
     public void Jester_Contains_JokeService()
     {
         //Arrange
@@ -33,5 +37,62 @@ public class JesterTests
         //Act
 
         //Assert
-    }*/
+        Assert.AreEqual<JokeService>(testJester.JokeService, jokeService);
+    }
+
+    [TestMethod]
+    public void Jester_Contains_JokeWriter()
+    {
+        //Arrange
+
+        //Act
+
+        //Assert
+        Assert.AreEqual<FunnyOut>(testJester.JokeWriter, jokeWriter);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void Jester_GivenNullJokeService_ThrowsNullException()
+    {
+        //Arrange
+
+        //Act
+        testJester = new(null!, jokeWriter);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void Jester_GivenNullJokeWriter_ThrowsNullException()
+    {
+        //Arrange
+        JokeService jokeService = new();
+
+        //Act
+        testJester = new(jokeService, null!);
+    }
+
+    [TestMethod]
+    public void Jester_GivenChuckNorrisJoke_ReturnTrue()
+    {
+        //Arrange
+        testString = "Hi, I'm Chuck Norris";
+
+        //Act
+
+        //Assert
+        Assert.IsTrue(((IJokeService)testJester.JokeService).CheckForChuckNorris(testString));
+    }
+
+    [TestMethod]
+    public void Jester_GivenNotChuckNorrisJoke_ReturnFalse()
+    {
+        //Arrange
+        testString = "Hi, I'm not that guy with all the jokes written about him";
+
+        //Act
+
+        //Assert
+        Assert.IsFalse(((IJokeService)testJester.JokeService).CheckForChuckNorris(testString));
+    }
 }
