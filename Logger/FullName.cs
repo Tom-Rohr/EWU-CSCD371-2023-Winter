0643f7a0e2c42baac28129646082cd33b5da243e
Tom-Rohr <trohr92@live.com>
2023-02-04 14:51:46 -0800

    Changed FullName from record struct to record class

--- Logger/FullName.cs
+++ Logger/FullName.cs
@@ -1,15 +1,15 @@
 ﻿namespace Logger;
 
 //A.)Creating a 'record struct' to define a custom value type as opposed to a 'record class' because...
-/////1.) FullNameRecord logically represents one value.
+/////1.) FullNameRecord logically represents up to three values.
 /////2.) It will be immutable.
-/////3.) It will consume less than 16-bytes of data.
-/////4.) It will not be boxed frequently, so memory overhead will be less than using a reference type.
+/////3.) It is possible/likely a full name will be greater than 16-bytes of data.
+/////4.) It will be boxed frequently by the Storage class, so memory overhead will be less than using a value type.
 /////5.) Nothing should need to inherit from this record.
 
 //B.)The Type is immutable because the record declaration is decorated with the 'readonly' modifier, meaning
 /////the positional parameters can only be set in the constructor during initialization and cannot be changed.
-public record struct FullName(string FirstName, string LastName, string? MiddleName = null)
+public record class FullName(string FirstName, string LastName, string? MiddleName = null)
 {
     public string FirstName { get; } = FirstName??throw new ArgumentNullException(nameof(FirstName));
     public string LastName { get; } = LastName??throw new ArgumentNullException(nameof(LastName));
