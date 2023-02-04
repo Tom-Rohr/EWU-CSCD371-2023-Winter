7770ba1a5c810e9981323ba2aee1a6e6a846acf9
Tom-Rohr <trohr92@live.com>
2023-02-04 14:10:10 -0800

    created abstract Entity record class, implements Id but not Name.

---
+++ Logger/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger;
+
+public abstract record class Entity : IEntity
+{
+    public abstract string Name { get; set; }
+    public Guid Id { get; init; }
+}
