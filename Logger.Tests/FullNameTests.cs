8f258bea232e120a0ef840ec011c8f91176e5ff0
Tom-Rohr <trohr92@live.com>
2023-02-04 14:52:57 -0800

    minor adjustment to FullName unit tests

--- Logger.Tests/FullNameTests.cs
+++ Logger.Tests/FullNameTests.cs
@@ -22,14 +22,14 @@ public class FullNameTests
     [ExpectedException(typeof(ArgumentNullException))]
     public void FullName_NullFirstName_ThrowsException()
     {
-        FullName testRecord = new FullName(FirstName: null, LastName: "Rohr", MiddleName: "Stephen");
+        FullName testRecord = new FullName(FirstName: null!, LastName: "Rohr", MiddleName: "Stephen");
     }
 
     [TestMethod]
     [ExpectedException(typeof(ArgumentNullException))]
     public void FullName_NullLastName_ThrowsException()
     {
-        FullName testRecord = new FullName(FirstName: "Thomas", LastName: null, MiddleName: "Stephen");
+        FullName testRecord = new FullName(FirstName: "Thomas", LastName: null!, MiddleName: "Stephen");
     }
 
     [TestMethod]
