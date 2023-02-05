857fbe1c859ed9aacebbe8d199d6d43c4f3dda96
Tom-Rohr <trohr92@live.com>
2023-02-05 13:22:21 -0800

    Implemented Equals() for Book class

--- Logger/Book.cs
+++ Logger/Book.cs
@@ -9,6 +9,12 @@ namespace Logger;
 public record class Book(string Title, string Author, int YearPublished) : Entity
 {
     public override string Name { get; init; } = Title ?? throw new ArgumentNullException(nameof(Title));
-    public virtual bool Equals(Book? other) => (Name).Equals(other?.Name); 
+    public string Author { get; init; } = Author?? throw new ArgumentNullException(nameof(Author));
+    public int YearPublished { get; init; } = YearPublished;
+    public virtual bool Equals(Book? other)
+    {
+        if (other is null) return false;
+        return (Name, Author, YearPublished) == (other?.Name, other?.Author, other?.YearPublished);
+    }
     public override int GetHashCode() =>  Title.GetHashCode();
 }
