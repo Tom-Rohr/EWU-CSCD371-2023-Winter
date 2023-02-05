643bf3642d62ade0f0702486740c65c3dce37eec
Tom-Rohr <trohr92@live.com>
2023-02-04 21:38:40 -0800

    Added unit test for overriden Equals() method, which is meant to check for value equality. Currently fails

--- Logger.Tests/StudentTests.cs
+++ Logger.Tests/StudentTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
@@ -12,11 +13,23 @@ namespace Logger.Tests;
 public class StudentTests
 {
     [TestMethod]
-    public void Student_SetsFullName_Success()
+    public void Student_SetsFullNameAndStanding_Success()
     {
         FullName testName = new("Michael","Scott","Gary");
-        Employee testStudent = new(testName);
+        Student testStudent = new(testName, Standing.Senior);
 
         Assert.AreEqual(testStudent.FName.ToString(), "Michael Gary Scott");
+        Assert.AreEqual(testStudent.studentStanding, Standing.Senior);
+    }
+
+    [TestMethod]
+    public void Student_ValueBasedEquality_True()
+    {
+        FullName testName = new("Michael", "Scott", "Gary");
+        Student testStudent = new(testName, Standing.Senior);
+        Student testStudent2 = new(testName, Standing.Senior);
+
+        Assert.IsTrue(testStudent2.Equals(testStudent));
+        //Assert.AreEqual(testStudent, testStudent2);
     }
 }
