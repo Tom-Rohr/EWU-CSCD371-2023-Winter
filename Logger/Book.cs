1ce4f4f13b60d882ffe701a15b852bf28b788b55
Tom-Rohr <trohr92@live.com>
2023-02-05 15:44:37 -0800

    Changed implementation of GetHashCode() and changed property 'Author' from a string to a FullName record class.

--- Logger/Book.cs
+++ Logger/Book.cs
@@ -1,9 +1,9 @@
 ﻿namespace Logger;
 
-public record class Book(string Title, string Author, int YearPublished) : Entity
+public record class Book(string Title, FullName Author, int YearPublished) : Entity
 {
     public override string Name { get; init; } = Title ?? throw new ArgumentNullException(nameof(Title));
-    public string Author { get; init; } = Author?? throw new ArgumentNullException(nameof(Author));
+    public FullName Author { get; init; } = Author?? throw new ArgumentNullException(nameof(Author));
     public int YearPublished { get; init; } = YearPublished;
 
     public virtual bool Equals(Book? other)
@@ -12,5 +12,5 @@ public record class Book(string Title, string Author, int YearPublished) : Entit
         return (Name, Author, YearPublished) == (other?.Name, other?.Author, other?.YearPublished);
     }
 
-    public override int GetHashCode() =>  Title.GetHashCode();
+    public override int GetHashCode() =>  HashCode.Combine(Title.GetHashCode(), Author.GetHashCode()) + YearPublished;
 }
