d47e267d953d58e97b54f2691a18a7e5b275db6f
Tom-Rohr <trohr92@live.com>
2023-02-04 21:36:56 -0800

    added enum Standing. Added Standing as a positional parameter to Student. Changed implementation of overriden ToString() method.

---
+++ Logger/Standing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger;
+
+public enum Standing
+{
+    None = 0,
+    Freshman,
+    Sophmore,
+    Junior,
+    Senior,
+    Graduate
+}
--- Logger/Student.cs
+++ Logger/Student.cs
@@ -7,11 +7,16 @@ using System.Xml.Linq;
 
 namespace Logger
 {
-    public record class Student(FullName FName) : Entity
+    public record class Student(FullName FName, Standing studentStanding) : Entity
     {
-        public override string Name { get; set; } = FName.ToString() ?? throw new ArgumentNullException(nameof(FName));
+        public override string Name { get; init; } = FName.ToString() ?? throw new ArgumentNullException(nameof(FName));
+        public Standing studentStanding { get; set; } = studentStanding; //TO DO do we need to perform a check on this enum?
 
-        public virtual bool Equals(Student? other) => (Name, Id).Equals((other?.Name, other?.Id)); //Can we do this without allowing nullability?
-        public override int GetHashCode() => HashCode.Combine(Name.GetHashCode(), Id.GetHashCode());
+        public override string ToString()
+        {
+            return String.Format("{0} {1} {2}",FName.FirstName, FName.MiddleName, FName.LastName);
+        }
+        public virtual bool Equals(Student? other) => (Name, studentStanding).Equals((other?.Name, other?.studentStanding)); //Can we do this without allowing nullability?
+        public override int GetHashCode() => HashCode.Combine(Name.GetHashCode(), studentStanding.GetHashCode());
     }
 }
