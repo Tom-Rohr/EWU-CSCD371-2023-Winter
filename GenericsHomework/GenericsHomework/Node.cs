1bec4cd609ae7e956458585ea979a88278318793
Tom Rohr <trohr92@live.com>
2023-02-11 13:49:25 -0800

    Renamed type parameter

--- GenericsHomework/GenericsHomework/Node.cs
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -1,21 +1,21 @@
 ﻿namespace GenericsHomework;
 
-public class Node<TValue>
+public class Node<TItem>
 {
-    public TValue Item { get; init; }
-    public Node<TValue> Next { get; private set; }
-    public TValue Prev { get; set; }
+    public TItem Item { get; init; }
+    public Node<TItem> Next { get; private set; }
+    public TItem Prev { get; set; }
 
-    public Node(TValue item)
+    public Node(TItem item)
     {
         Item = item ?? throw new ArgumentNullException(nameof(item));
         Next = this ?? throw new ArgumentNullException(nameof(item));//Initially points to itself, cannot be null per instructions
         Prev = Item ?? throw new ArgumentNullException(nameof(item));//Initially points to its own item.
     }
 
-    public Node<TValue> Append(TValue newItem) //TO DO always have newly created node point to first item that was added
+    public Node<TItem> Append(TItem newItem) //TO DO always have newly created node point to first item that was added
     {
-        Node<TValue> newNode = new(newItem); //Make new node      //Indicate this is not the tail
+        Node<TItem> newNode = new(newItem); //Make new node      //Indicate this is not the tail
         newNode.Prev = Item;                 //Will point to previous item (this.Item)
         Next = newNode;                      //Set current node's next to the new node
         return newNode;                      //Return the new Node, which is now the head of the list
