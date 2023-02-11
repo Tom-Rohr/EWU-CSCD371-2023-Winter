078b65a77d1ecb5c62b5148bb3b90def39f7f6d6
Tom Rohr <trohr92@live.com>
2023-02-11 15:38:18 -0800

    Added a CLL class and Append method

--- GenericsHomework/GenericsHomework/Node.cs
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -4,39 +4,19 @@ public class Node<TItem>
 {
     public TItem Item { get; init; }
     public Node<TItem> Next { get; private set; }
-    public TItem Prev { get; set; }
 
     public Node(TItem item)
     {
         Item = item ?? throw new ArgumentNullException(nameof(item));
         Next = this ?? throw new ArgumentNullException(nameof(item));//Initially points to itself, cannot be null per instructions
-        Prev = Item ?? throw new ArgumentNullException(nameof(item));//Initially points to its own item.
     }
 
-    public Node<TItem> Append(TItem newItem) //TO DO always have newly created node point to first item that was added
+    public void Append(GenericCircularlyLL<TItem> list)
     {
-        Node<TItem> newNode = new(newItem);  //Make new node
-        newNode.Next = Next;                
-        newNode.Prev = Item;                 //Will point to previous item (this.Item)
-        Next = newNode;                      //Set current node's next to the new node
-        return newNode;                      //Return the new Node, which is now the head of the list
-
-        /*TODO: Write a Exists method to check if what we are trying to add is already in the list.
-                Call the method before the return statement and throw an exception if there is a duplicate*/
+        Next = list.Current!;
+        list.Head!.Next = list.Current!;
     }
 
-    /*
-    public bool Exists(TValue item)
-    {
-        return false;
-    }
-    */
-    /*
-    public void Clear()
-    {
-
-    }
-    */
     public override string? ToString()
     {
         return Item!.ToString(); //Better way to handle than using '!'?
