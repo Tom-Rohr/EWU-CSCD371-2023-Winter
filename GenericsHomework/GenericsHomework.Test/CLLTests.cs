19773c1bcf3c9163317d9f054e2d621a2057aa77
Tom Rohr <trohr92@live.com>
2023-02-11 15:54:01 -0800

    Created test file for CLL tests

---
+++ GenericsHomework/GenericsHomework.Test/CLLTests.cs
@@ -0,0 +1,14 @@
+namespace GenericsHomework.Test;
+
+[TestClass]
+public class CLLTests
+{
+    [TestMethod]
+    public void CLL_FieldsNullWhenEmpty_True()
+    {
+        GenericCircularlyLL<int> list = new();
+
+        Assert.IsNull(list.Head);
+        Assert.IsNull(list.Current);
+    }
+}
