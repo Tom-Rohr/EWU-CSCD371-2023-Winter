5a706ee2042a9ee0ea4dd32e3a87062d3b7c716d
Tom Rohr <trohr92@live.com>
2023-02-18 13:54:16 -0800

    Changed Program delegate properties to default to Console methods
    Now test gets stuck executing when trying to instantiate a Program with object initializers

--- Calculate/Calculate/Program.cs
+++ Calculate/Calculate/Program.cs
@@ -2,9 +2,35 @@
 
 public class Program
 {
-    public Func<string, string>? WriteLine { get; init; }
-    public Func<string, string>? ReadLine { get; init; }
+    public Func<string, string>? WriteLine
+    {
+        get
+        {
+            return WriteLine;
+        }
+        init
+        {
+            Console.WriteLine();
+        } 
+    }
 
+    public Func<string, string>? ReadLine 
+    {
+        get
+        {
+            return ReadLine;
+        }
+        init
+        {
+            Console.ReadLine();
+        }
+    }
+
+    public Program(Func<string, string>? writeLine, Func<string, string>? readLine)
+    {
+        WriteLine = writeLine;
+        ReadLine = readLine;
+    }
 
     public Program() { }
     public static void Main(string[] args)
